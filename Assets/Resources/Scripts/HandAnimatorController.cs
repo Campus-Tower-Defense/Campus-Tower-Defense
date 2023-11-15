5efcf939325232babc4c18dd0448caf02e9ed27c
Fabian <92756119+fpechta@users.noreply.github.com>
2023-11-15 16:33:18 +0100

    Added hands and Animationscript

---
+++ Assets/Resources/Scripts/HandAnimatorController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimatorController : MonoBehaviour
+{
+     [SerializeField] private InputActionProperty triggerAction;
+     [SerializeField] private InputActionProperty gripAction;
+     
+     private Animator anim;
+     
+     private void Start() {
+         anim = GetComponent<Animator>();
+     }    
+     
+     private void update() {
+         float triggerValue = triggerAction.action.ReadValue<float>();
+         float gripValue = gripAction.action.ReadValue<float>();
+         
+         anim.SetFloat("Trigger", triggerValue);
+         anim.SetFloat("Grip", gripValue);
+     }
+     
+}
