3e2f25b7f271039191f93e7a69f1a2989691c883
Eric <soprah@gmx.de>
2023-11-21 14:57:28 +0100

    Added basic waypoint system

---
+++ Assets/Resources/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   public float speed = 10f;
+   private Transform target;
+   private int wavepointIndex = 0;
+
+   void Start()
+   {
+      target = Waypoints.points[0];
+   }
+
+   void Update()
+   {
+      
+      // Zum Wegpunkt bewegen
+      Vector3 dir = target.position - transform.position;
+      transform.Translate(dir.normalized * (speed * Time.deltaTime), Space.World);
+
+      // Prüfen, ob Enemy am Wegpunkt angekommen ist
+      if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+      {
+         GetNextWayPoint();
+      }
+   }
+
+   // Nächsten Wegpunkt als Ziel nehmen
+   void GetNextWayPoint()
+   {
+      if (wavepointIndex >= Waypoints.points.Length - 1)
+      {
+         Destroy(gameObject);
+         // Man muss hier ein "return" einfügen, weil die "Destroy()" etwas Zeit braucht, aber das Programm..
+         // ... schon die nächsten Programmzeile bearbeitet/liest und deshalb der Fehler auftaucht "Array out of Bounce/Index"
+         return;
+      }
+      wavepointIndex++;
+      target = Waypoints.points[wavepointIndex];
+   }
+}
