5445051a3805afa803b33fd8b268ee31769fecac
Fabian <92756119+fpechta@users.noreply.github.com>
2023-11-24 01:00:39 +0100

    Added basic ability to place towers
    ToDo: UI popup to select which tower will be placed and gridfixing

---
+++ Assets/Resources/Scripts/TowerPlacement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    public GameObject towerPrefab; // Assign your tower prefab in the Inspector
+    public LayerMask gridLayer; // Set the layer for your grid objects
+    public float towerOffset = 0.1f; // Offset to avoid z-fighting
+
+    private Dictionary<Vector3, GameObject> occupiedGridCells = new Dictionary<Vector3, GameObject>();
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            PlaceTower();
+        }
+    }
+
+    void PlaceTower()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, gridLayer))
+        {
+            Vector3 towerPosition = hit.point;
+            towerPosition.y = towerPrefab.transform.position.y; // Align with the ground
+
+            // Round the position to prevent floating-point inaccuracies
+            towerPosition = RoundPosition(towerPosition);
+
+            // Check if the grid cell is already occupied
+            if (!occupiedGridCells.ContainsKey(towerPosition))
+            {
+                // No tower in this cell, instantiate the new tower
+                GameObject newTower = Instantiate(towerPrefab, towerPosition, Quaternion.identity);
+
+                // Mark this grid cell as occupied
+                occupiedGridCells.Add(towerPosition, newTower);
+            }
+            else
+            {
+                // There's already a tower in this cell, handle accordingly (e.g., display a message)
+                Debug.Log("Cannot place tower here - cell is already occupied.");
+            }
+        }
+    }
+
+    Vector3 RoundPosition(Vector3 position)
+    {
+        return new Vector3(Mathf.Round(position.x), Mathf.Round(position.y), Mathf.Round(position.z));
+    }
+}
