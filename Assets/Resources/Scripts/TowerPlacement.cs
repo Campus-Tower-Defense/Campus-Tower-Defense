85448106632e6cedc5df7d51434b134f47087253
Fabian <92756119+fpechta@users.noreply.github.com>
2023-12-03 17:27:33 +0100

    Added UI functionality to the Grid
    Lets the player choose between towers

--- Assets/Resources/Scripts/TowerPlacement.cs
+++ Assets/Resources/Scripts/TowerPlacement.cs
@@ -1,54 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerPlacement : MonoBehaviour
 {
-    public GameObject towerPrefab; // Assign your tower prefab in the Inspector
     public LayerMask gridLayer; // Set the layer for your grid objects
     public float towerOffset = 0.1f; // Offset to avoid z-fighting
 
+    private GameObject selectedTowerPrefab; // The selected tower prefab
     private Dictionary<Vector3, GameObject> occupiedGridCells = new Dictionary<Vector3, GameObject>();
+
+    // UI variables
+    public Canvas towerSelectionCanvas;
+    public Button tower1Button;
+    public Button tower2Button;
+
+    private void Start()
+    {
+        // Set up UI button click listeners
+        tower1Button.onClick.AddListener(() => SelectTowerPrefab("Tower1"));
+        tower2Button.onClick.AddListener(() => SelectTowerPrefab("Tower2"));
+
+        // Initially hide the tower selection UI
+        towerSelectionCanvas.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButtonDown("Fire1"))
         {
-            PlaceTower();
-        }
-    }
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
 
-    void PlaceTower()
-    {
-        Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, gridLayer))
-        {
-            Vector3 towerPosition = hit.point;
-            towerPosition.y = towerPrefab.transform.position.y; // Align with the ground
-
-            // Round the position to prevent inaccuracies
-            towerPosition = RoundPosition(towerPosition);
-
-            // Check if the grid cell is already occupied
-            if (!occupiedGridCells.ContainsKey(towerPosition))
+            // Check if the ray hits an object on the grid layer
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, gridLayer))
             {
-                // No tower in this cell, instantiate the new tower
-                GameObject newTower = Instantiate(towerPrefab, towerPosition, Quaternion.identity);
+                Vector3 hitPoint = hit.point;
 
-                // Mark this grid cell as occupied
-                occupiedGridCells.Add(towerPosition, newTower);
-            }
-            else
-            {
-                Debug.Log("Cannot place tower here - cell is already occupied.");
+                // Show the tower selection UI
+                ShowTowerSelectionUI(hitPoint);
             }
         }
     }
 
-    Vector3 RoundPosition(Vector3 position)
+    void ShowTowerSelectionUI(Vector3 position)
     {
-        return new Vector3(Mathf.Round(position.x), Mathf.Round(position.y), Mathf.Round(position.z));
+        // Set the tower selection UI position to be above the grid position
+        towerSelectionCanvas.transform.position = position + new Vector3(0, 1.5f, 0);
+
+        // Activate the tower selection UI
+        towerSelectionCanvas.gameObject.SetActive(true);
+    }
+
+    void SelectTowerPrefab(string towerPrefabName)
+    {
+        // Find the tower prefab based on the name
+        selectedTowerPrefab = Resources.Load<GameObject>(towerPrefabName);
+
+        if (selectedTowerPrefab != null)
+        {
+            // Hide the tower selection UI
+            Debug.Log("Tower prefab selected: " + towerPrefabName);
+            towerSelectionCanvas.gameObject.SetActive(false);
+            
+        }
+        else
+        {
+            Debug.LogError("Tower prefab not found: " + towerPrefabName);
+        }
+    }
+
+    void PlaceTower(Vector3 towerPosition)
+    {
+        // Check if the grid cell is already occupied
+        if (!occupiedGridCells.ContainsKey(towerPosition))
+        {
+            // Calculate the middle of the grid cell
+            Vector3 middleOfCell = towerPosition + new Vector3(0.5f, 0f, 0.5f);
+
+            // Instantiate the selected tower at the middle of the cell
+            GameObject newTower = Instantiate(selectedTowerPrefab, middleOfCell, Quaternion.identity);
+
+            // Mark this grid cell as occupied
+            occupiedGridCells.Add(towerPosition, newTower);
+        }
+        else
+        {
+            // There's already a tower in this cell, handle accordingly (e.g., display a message)
+            Debug.Log("Cannot place tower here - cell is already occupied.");
+        }
     }
 }
