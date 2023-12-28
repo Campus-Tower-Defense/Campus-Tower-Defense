8f44e27a70fcd78967f6a307a6386eed6c07fac3
Eric <soprah@gmx.de>
2023-12-28 22:49:14 +0100

    Spawning enenemies in a 1sec intervall + amount of enemies to spawn is given by parameter

--- Assets/Resources/Scripts/EnemySpawner.cs
+++ Assets/Resources/Scripts/EnemySpawner.cs
@@ -1,23 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-
     public GameObject enemyPrefab;
     public Transform[] spawnPoints;
+    public float spawnInterval = 1f;
+    public int amountEnemy = 5;
 
     private void Start()
     {
-        SpawnEnemies();
+        StartCoroutine(SpawnEnemies());
     }
 
-    private void SpawnEnemies()
+    private IEnumerator SpawnEnemies()
     {
         foreach (Transform spawnPoint in spawnPoints)
+        {
+            StartCoroutine(SpawnEnemiesAtPoint(spawnPoint));
+        }
+
+        // Warte, bis alle Spawnpunkte fertig sind
+        yield return new WaitForSeconds(amountEnemy * spawnInterval);
+    }
+
+    private IEnumerator SpawnEnemiesAtPoint(Transform spawnPoint)
+    {
+        for (int i = 0; i < amountEnemy; i++)
         {
             GameObject enemyInstance = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
             enemyInstance.transform.SetParent(spawnPoint);
             enemyInstance.AddComponent<EnemyMovement>();
+
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
